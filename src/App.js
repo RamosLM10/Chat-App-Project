f1f066851c47adcf7285a89879ba2a11af38124f
Luis Ramos <luismiguel.ramos@yahoo.com.au>
2022-03-09 21:00:35 +1100

    added styling and functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < ChatEngine
+        height= "100vh"
+        projectID= "62ff7bf3-1146-4f86-9f3d-57884bcfd1b5"   
+        userName= "Luis Ramos"
+        userSecret= "Luis"
+    />
   );
 }
 
