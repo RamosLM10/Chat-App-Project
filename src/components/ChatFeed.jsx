1499bbc0a431ff76385ab30b25bb195ed811996a
Luis Ramos <luismiguel.ramos@yahoo.com.au>
2022-03-12 03:10:46 +1100

    added functionality in chatfeed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -18,16 +18,48 @@ const ChatFeed = (props) => {
 
       // if index return 0 then return null, else return last message
       const lastMessageKey = index === 0 ? null : keys[index - 1];
+      
+      const isMyMessage = userName === message.sender.username;
 
-      const isMyMessage = username === message.sender.username;
+      return (
+        <div key={`msg_${style}`} style={{ width: '100%' }}>
+          <div className="message-block">
+            
+            {/* if ismymessage then render mymessage, but if not, render theirmessage*/}
+            {
+              isMyMessage
+              ?
+              <MyMessage />
+              :
+              <TheirMessage />
+            }
+          </div>
+          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft : isMyMessage ? '0px' : '68px' }}>
+            read receipts
+          </div>
+        </div>
+      )
     })
   }
 
-  renderMessages()
+  // if no chat
+  if(!chat) return 'Please wait...'
 
   return (
-    <div>
-      Chatfeed
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat.title}</div>
+        <div className="chat-subtitle">
+          {/* using dynamic logic */}
+          {chat.people.map((person) => ` ${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      {/* create a self-closing div for space */}
+      <div style={{ height: '100px' }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
     </div>
   );
 }
