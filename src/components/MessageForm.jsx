853904b0060497145a8ed796014a86643311cb5b
Luis Ramos <luismiguel.ramos@yahoo.com.au>
2022-03-30 19:12:11 +1100

    modified colorway on app.css and did more work on messageform.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { sendMessage, isTyping } from "react-chat-engine"
+import { SendOutlined, PictureOutlined } from "@ant-design/icons"
 
 const MessageForm = (props) => {
   const [value, setValue] = useState('')
@@ -15,11 +16,16 @@ const MessageForm = (props) => {
 
     setValue('');
   }
+
   const handleChange = (event) => {
     // where the value of the input is stored in
     setValue(event.target.value);
 
     isTyping(props, chatId);
+  }
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: '' })
   }
 
   return (
@@ -31,6 +37,21 @@ const MessageForm = (props) => {
         onChange={handleChange}
         onSumbit={handleSubmit}
       />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon"/>
+        </span>
+      </label>
+      <input  
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: 'none' }}
+        onChange={handleUpload}
+      />
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon"/>
+      </button>
     </form>
   )
 }
