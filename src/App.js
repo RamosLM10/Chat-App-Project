4d7c1832d9ce94d3f1de3d377f164314b6a98e16
Luis Ramos <luismiguel.ramos@yahoo.com.au>
2022-03-31 03:39:26 +1100

    made some alterations on chatfeed and app.js and changed some colors on app.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
 import  ChatFeed  from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  // if there is no username
+  if (!localStorage.getItem('username')) return <LoginForm />
+  
   return (
-    < ChatEngine
-        height= "100vh"
-        projectID= "62ff7bf3-1146-4f86-9f3d-57884bcfd1b5"   
-        userName= "Luis Ramos"
-        userSecret= "Luis"
-        renderChatFeed= {(chatAppProps) => <ChatFeed { ...chatAppProps }  />}
+    <ChatEngine
+      height= "100vh"
+      projectID= "62ff7bf3-1146-4f86-9f3d-57884bcfd1b5"   
+      userName= {localStorage.getItem('username')}
+      userSecret= {localStorage.getItem('password')}
+      renderChatFeed= {(chatAppProps) => <ChatFeed { ...chatAppProps }  />}
     />
   );
 }
